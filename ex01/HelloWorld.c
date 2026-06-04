472f5e5e6fd830af9014b48f60781ed0c236b8c5
unknown <your_email@example.com>
2026-06-04 13:06:35 +0800

    第二次提交：增加键盘输入与输出功能

--- ex01/HelloWorld.c
+++ ex01/HelloWorld.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!\n");
+    char inputStr[100]; // 定义一个字符数组来存储输入的内容
+
+    // 提示用户输入
+    printf("请输入一些内容: ");
+    
+    // 接收键盘输入（遇到空格或换行会停止）
+    scanf("%99s", inputStr); 
+
+    // 显示输出键盘输入内容
+    printf("你刚才输入的内容是: %s\n", inputStr);
+
     return 0;
 }
