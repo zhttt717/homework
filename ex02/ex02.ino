8b85e928d918a585b9b999569cf58088a9c23a9b
unknown <your_email@example.com>
2026-06-20 17:23:44 +0800

    feat(ex02): 使用 millis() 完成 1Hz 稳定闪烁逻辑

--- ex02/ex02.ino
+++ ex02/ex02.ino
@@ -1,6 +1,6 @@
-const int ledPin = 2; // ESP32 板载 LED 引脚
-unsigned long previousMillis = 0; 
-const long interval = 500; // 500ms 亮，500ms 灭
+const int ledPin = 2; 
+unsigned long previousMillis = 0;
+const long interval = 500; 
 int ledState = LOW;
 
 void setup() {
@@ -8,5 +8,18 @@ void setup() {
 }
 
 void loop() {
-  // 核心逻辑暂时留空
+  unsigned long currentMillis = millis(); // 获取当前系统运行的毫秒数
+
+  // 如果当前时间减去上次记录的时间，大于等于我们设置的间隔(500ms)
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis; // 更新时间记录
+
+    // 翻转 LED 的状态
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(ledPin, ledState); // 输出状态到引脚
+  }
 }
